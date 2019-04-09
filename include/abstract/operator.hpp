e5748e3c99712c8edf3a50e78aaa8ced321dfa13
bryce_workmac <westheb@iastate.edu>
2019-04-09 16:14:53 -0500

    Started copying over elements from private repo, but considering full redesign

--- include/abstract/operator.hpp
+++ include/abstract/operator.hpp
@@ -11,12 +11,72 @@
 #ifndef QCCG_ABSTRACT_OPERATOR_HPP_
 #define QCCG_ABSTRACT_OPERATOR_HPP_
 
+#include "libaccint.hpp"  // BasisSet
+
+// Convenience typedef(s)
+typedef libaccint::host::core::BasisSet BasisSet;
+
 namespace qccg {
 
   namespace abstract {
 
+    /*! \class Operator
+     *  \ingroup TODO:ADD_GROUP
+     * 
+     *  \brief The abstract second-quantization Operator class
+     * 
+     *  \details TODO
+     * 
+     *  \author Bryce M. Westheimer
+     * 
+     *  \date Apr 9, 2019 
+     * 
+     */ 
     class Operator {
-      // TODO: Copy
+    
+      public:
+
+      /***** Constructor(s) and Destructor(s) *****/
+
+        Operator();
+        // TODO: Etc.
+        ~Operator();
+
+      /***** Public Member Functions *****/
+
+        // TODO
+
+      /***** Public Member Variables *****/
+
+        // TODO
+
+      /***** Operator Overloads *****/
+
+        // TODO
+      
+      protected:
+
+      /***** Protected Member Functions *****/
+
+        // TODO
+
+      /***** Protected Member Variables *****/
+
+        // TODO
+
+      private:
+
+      /***** Private Member Functions *****/
+
+        // TODO
+
+      /***** Private Member Variables *****/
+
+        // Basis set that an operator is expanded in
+        BasisSet basis_set_;
+        
+        // TODO: Etc.
+
     };
 
   } // namespace qccg::abstract
